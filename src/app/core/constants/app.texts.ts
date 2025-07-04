c804ad40bfcdbf18a8d2e6bee43849c2a5d06f29
Oscar Perez <ojperez@bancolombia.com.co>
2025-07-04 15:14:54 -0500

    feat(auth): ocultar botón de creación de propiedad para clientes, mejorar control de roles y refactor de constantes para evitar datos quemados

--- src/app/core/constants/app.texts.ts
+++ src/app/core/constants/app.texts.ts
@@ -6,5 +6,14 @@ export const AppTexts = {
   LOGIN_TITLE: 'Iniciar sesión',
   LOGIN_BUTTON: 'Ingresar',
   EMAIL_LABEL: 'Email',
-  PASSWORD_LABEL: 'Contraseña'
+  PASSWORD_LABEL: 'Contraseña',
+  ERROR_ROLES: 'Tu cuenta no tiene roles asignados. Contacta al administrador.',
+  ERROR_CREDENTIALS: 'Credenciales incorrectas',
+  ERROR_CHARGE_PROPERTYS: 'No se pudieron cargar tus propiedades',
+  ERROR_CHARGE_PROPERTYS_AVAILABLES: 'No se pudieron cargar las propiedades disponibles',
+  CONFIRM_DELETE_USER: '¿Estás seguro de eliminar este usuario?',
+  CONFORM_CREATE_PROPERTY: '✅ Propiedad creada correctamente',
+  DELETE_USER_SUCCESS: 'Usuario eliminado correctamente',
+  LOGOUT: 'Sesión finalizada',
+
 };
